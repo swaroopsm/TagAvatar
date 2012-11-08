cc1c160b3de9a45aa310ebfc0635dfb0ea81225a
Swaroop SM <swaroop.striker@gmail.com>
2012-11-09 02:01:26 +0530

    moved using refactor option

---
+++ TagAvatar/src/local/tagavatar/server/default_settings.java
@@ -0,0 +1,27 @@
+package local.tagavatar.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.sql.*;
+
+public class default_settings {
+	
+	/**
+	 * @param args
+	 */
+	private String mysql_username="";
+	private String mysql_password="";
+	private String mysql_db="";
+	private String mysql_url="jdbc:mysql://localhost:3306/"+mysql_db;
+	private Connection con;
+	
+	public void test(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection(this.mysql_url,this.mysql_username,this.mysql_password);
+		}catch(Exception e){
+			
+		}
+	}
+}
