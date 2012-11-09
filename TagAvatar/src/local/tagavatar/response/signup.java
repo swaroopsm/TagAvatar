d91739344b7747fba9a314d1d07efd7baf83d788
Swaroop SM <swaroop.striker@gmail.com>
2012-11-09 11:29:19 +0530

    request/response handler for signing up

---
+++ TagAvatar/src/local/tagavatar/response/signup.java
@@ -0,0 +1,47 @@
+package local.tagavatar.response;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import local.tagavatar.server.Settings;
+import local.tagavatar.server.Users;
+
+/**
+ * Servlet implementation class signup
+ */
+@WebServlet("/signup")
+public class signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public signup() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		Users u=new Users();
+		out.println(u.create(request.getParameter("name"), request.getParameter("email"), request.getParameter("username"), request.getParameter("password")));
+	}
+
+}
