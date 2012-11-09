0bf5a2e0233433c2491033a07f4a6c77275efcad
Swaroop SM <swaroop.striker@gmail.com>
2012-11-09 21:42:01 +0530

    display loader img during data insert

--- TagAvatar/WebContent/js/tagavatar.js
+++ TagAvatar/WebContent/js/tagavatar.js
@@ -2035,11 +2035,13 @@ $("form#signup_form").live("submit", function(){
 	if(name=='' || email=='' || username=='' || pwd==''){
 		$("#js-messages").html("<br><center><span style='margin-left: 25%;' class='span5 alert alert-danger'><a class='close' data-dismiss='alert' href='#'>&times;</a>Please fill all fields...</span></center>").hide().fadeIn(500);
 	}else{
+		$("#loader").show();
 		$.post("/TagAvatar/signup", {name: name, email: email, username: username, password: pwd},
 				function(data){
 			$("#js-messages").html("<br><center><span style='margin-left: 25%;' class='span5 alert alert-success'><a class='close' data-dismiss='alert' href='#'>&times;</a>"+data+"</span></center>").hide().fadeIn(500);
 		});
 	}
+	$("#loader").fadeOut(300);
 	return false;
 });
 
