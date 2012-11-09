5c456c1998aca887cda4cc9af3ca586924be852b
Swaroop SM <swaroop.striker@gmail.com>
2012-11-09 22:48:09 +0530

    servlet that handles login request

---
+++ TagAvatar/src/local/tagavatar/response/login.java
@@ -0,0 +1,53 @@
+package local.tagavatar.response;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import local.tagavatar.server.Session;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username=request.getParameter("inputLoginID");
+		String pwd=request.getParameter("inputPassword");
+		Session s=new Session();
+		String stat=s.login(username, pwd);
+		if(stat=="0"){
+			RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+			request.setAttribute("msg", "error");
+			rd.forward(request, response);
+		}
+	}
+
+}
