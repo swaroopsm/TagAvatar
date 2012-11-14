a2848875bab8d692a89e1e1231d6994cdb82ce2d
Swaroop SM <swaroop.striker@gmail.com>
2012-11-14 15:10:45 +0530

    create method using PreparedStatements

--- TagAvatar/src/local/tagavatar/server/Photos.java
+++ TagAvatar/src/local/tagavatar/server/Photos.java
@@ -13,5 +13,20 @@ public class Photos {
 		this.con=s.get_connection();
 	}
 	
+	public Boolean create(String title, String desc, String photo, String username){
+		String sql="INSERT INTO photos(`title`,`desc`,`photo`,`user_id`,`datetime`) VALUES(?,?,?,?,?)";
+		try{
+			PreparedStatement ps=this.con.prepareStatement(sql);
+			ps.setString(1, title);
+			ps.setString(2, desc);
+			ps.setString(3, photo);
+			ps.setString(4, username);
+			ps.setString(5, "");
+			ps.executeUpdate();
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
 	
 }
