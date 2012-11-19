e2cf8d0bfdecd7bd9261d0d2f85ee85a52b86cfd
Swaroop SM <swaroop.striker@gmail.com>
2012-11-20 01:23:09 +0530

    defined create function

--- TagAvatar/src/local/tagavatar/server/Likes.java
+++ TagAvatar/src/local/tagavatar/server/Likes.java
@@ -12,4 +12,18 @@ public class Likes {
 		Settings s=new Settings();
 		this.con=s.get_connection();
 	}
+	
+	public String create(String username, int photo){
+		String sql="INSERT INTO likes(user_id,photo) VALUES('"+username+"',"+photo+")";
+		JSONObject json=new JSONObject();
+		try{
+			Statement st=this.con.createStatement();
+			st.execute(sql);
+			json.put("status", true);
+			return json.toString();
+		}catch(Exception e){
+			return (json.put("status", false)).toString();
+		}
+	}
+	
 }
