dcbda67a43b7e076280870565ba590c18e0e0563
Swaroop SM <swaroop.striker@gmail.com>
2012-11-20 18:04:13 +0530

    defined create method

--- TagAvatar/src/local/tagavatar/server/Dislikes.java
+++ TagAvatar/src/local/tagavatar/server/Dislikes.java
@@ -2,6 +2,7 @@ package local.tagavatar.server;
 
 import java.sql.*;
 import local.tagavatar.server.Settings;
+import local.tagavatar.server.Likes;
 
 public class Dislikes {
 	
@@ -10,6 +11,20 @@ public class Dislikes {
 	public Dislikes(){
 		Settings s=new Settings();
 		this.con=s.get_connection();
+	}
+	
+	public int create(int photo, String username){
+		String sql="INSERT INTO dislikes(`photo`,`user_id`) VALUES("+photo+",'"+username+"')";
+		Likes l=new Likes();
+		try{
+			if(l.delete(photo, username)==1){
+				Statement st=this.con.createStatement();
+				st.execute(sql);
+			}
+			return 1;
+		}catch(Exception e){
+			return 0;
+		}
 	}
 	
 }
